d1ef6f820bb5021b0986ed36c78391951c74ca40
Youraj singh <yourajsingh6113@gmail.com>
2026-07-21 16:16:46 +0530

    Time: 7 ms (29.25%), Space: 47.6 MB (5.98%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,23 +1,29 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        int count1 = 0;
-        int count2 = 0;
-        for(int i = 0; i < nums1.length; i++) {
-            for(int j = 0; j < nums2.length; j++) {
-                if(nums1[i] == nums2[j]) {
-                    count1++;
-                    break;
-                }
+        HashSet<Integer>set1=new HashSet<>();
+        HashSet<Integer>set2=new HashSet<>();
+
+        for(int i=0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            set2.add(nums2[i]);
+        }
+
+        int count1=0;
+        int count2=0;
+
+        for(int i=0;i<nums1.length;i++){
+            if(set2.contains(nums1[i])){
+                count1++;
             }
         }
-        for(int i = 0; i < nums2.length; i++) {
-            for(int j = 0; j < nums1.length; j++) {
-                if(nums2[i] == nums1[j]) {
-                    count2++;
-                    break;
-                }
+        for(int i=0;i<nums2.length;i++){
+            if(set1.contains(nums2[i])){
+                count2++;
             }
         }
-        return new int[]{count1, count2};
+
+        return new int []{count1,count2};
     }
 }
