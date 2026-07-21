48b8faa6ecdbabf9a6c319b6a2d6296ea30e69cc
Youraj singh <yourajsingh6113@gmail.com>
2026-07-21 15:46:07 +0530

    Time: 0 ms (100%), Space: 43.6 MB (77.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,6 +3,7 @@ class Solution {
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
+
         while (i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
@@ -13,8 +14,8 @@ class Solution {
             else{
                 nums1[k]=nums2[j];
 
-                k--;
                 j--;
+                k--;
             }
         }
         while(j>=0){
@@ -25,3 +26,5 @@ class Solution {
         }
     }
 }
+     
+    
