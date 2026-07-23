8a7dd0f2c18698d67dd8749dac0f53165e056645
Youraj singh <yourajsingh6113@gmail.com>
2026-07-23 12:59:42 +0530

    Time: 2 ms (42.35%), Space: 94.8 MB (15.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,44 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = prices[0];
+      int minPrice=prices[0];
+      int maxProfit=0;
+      for(int i=1;i<prices.length;i++){
+        int profit=prices[i]-minPrice;
+        if(profit>maxProfit){
+            maxProfit=profit;
+        }
+        else if(prices[i]<minPrice){
+            minPrice=prices[i];
+        }
+      }
+      return maxProfit;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int minPrice = prices[0];
         int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] < minPrice) {
@@ -10,6 +48,4 @@ class Solution {
                 maxProfit = Math.max(maxProfit, profit);
             }
         }
-        return maxProfit;
-    }
-}
+        return maxProfit;*/
