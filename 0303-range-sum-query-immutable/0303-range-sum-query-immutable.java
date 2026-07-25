4dc743af3648eea7a18e8ec53c52b59dbb376cdc
Youraj singh <yourajsingh6113@gmail.com>
2026-07-25 16:53:13 +0530

    Time: 8 ms (71.41%), Space: 47.9 MB (25.61%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -14,23 +14,3 @@ class NumArray {
         return prefix[right]-prefix[left-1];
     }
 }
-
-
-
-
-/*class NumArray {
-    int[] prefix;
-    public NumArray(int[] nums) {
-        prefix = new int[nums.length];
-        prefix[0] = nums[0];
-        for(int i = 1; i < nums.length; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
-    }
-    public int sumRange(int left, int right) {
-        if(left == 0) {
-            return prefix[right];
-        }
-        return prefix[right] - prefix[left - 1];
-    }
-}*/
