4d29fa5318e0b016e2d32d3f9d6b550d769254c1
Youraj singh <yourajsingh6113@gmail.com>
2026-07-26 16:12:01 +0530

    Time: 6 ms (82.59%), Space: 67.1 MB (74.22%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,14 +1,15 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            int value = Math.abs(nums[i]);
-            int naturalIndex=value-1;
-            nums[naturalIndex]=-Math.abs(nums[naturalIndex]);
+       List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
         }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                ans.add(i+1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
             }
         }
         return ans;
@@ -49,3 +50,30 @@ class Solution {
             }
         }
         return ans;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int value = Math.abs(nums[i]);
+            int naturalIndex=value-1;
+            nums[naturalIndex]=-Math.abs(nums[naturalIndex]);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                ans.add(i+1);
+            }
+        }
+        return ans; */
