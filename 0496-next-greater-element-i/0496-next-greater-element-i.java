e5c90c2c40a10bb260ce1848e66a6f98c04a5334
Youraj singh <yourajsingh6113@gmail.com>
2026-07-27 13:55:22 +0530

    Time: 4 ms (73.86%), Space: 45.5 MB (11.15%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map =new HashMap<>();
-        Stack<Integer> stack=new Stack<>();
-        for(int i=0;i<nums2.length;i++){
-            int current=nums2[i];
-            while(!stack.isEmpty()&&current>stack.peek()){
-                map.put(stack.peek(),current);
+       HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
                 stack.pop();
             }
-            stack.push(current);
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
         }
-        while(!stack.isEmpty()){
-            map.put(stack.peek(),-1);
-            stack.pop();
-        }  
-        int []ans=new int [nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            ans[i]=map.get(nums1[i]);
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
         }
         return ans;
     }
@@ -46,5 +45,26 @@ class Solution {
         int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             ans[i] = map.get(nums1[i]);
+        }
+        return ans; */
+
+
+        /* HashMap<Integer,Integer> map =new HashMap<>();
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<nums2.length;i++){
+            int current=nums2[i];
+            while(!stack.isEmpty()&&current>stack.peek()){
+                map.put(stack.peek(),current);
+                stack.pop();
+            }
+            stack.push(current);
+        }
+        while(!stack.isEmpty()){
+            map.put(stack.peek(),-1);
+            stack.pop();
+        }  
+        int []ans=new int [nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=map.get(nums1[i]);
         }
         return ans; */
